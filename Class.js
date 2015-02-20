af739f3eeaf268fb795f5898886c0f9664a9dd15
Denis Kolesnikov <d.kolesnikov@office.ngs.ru>
2015-02-20 17:52:20 +0600

    Added jade template

--- Class.js
+++ Class.js
@@ -3,6 +3,7 @@ import SuperClass from './SuperClass';
 class TestClass extends SuperClass {
     constructor() {
         this.template = require('raw!./templates/template.tpl');
+        this.jadeTemplate = require('raw!jade-html!./templates/template.jade');
         console.log(this.template);
     }
 }
